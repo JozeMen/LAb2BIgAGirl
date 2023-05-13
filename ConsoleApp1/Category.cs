52b492117c7890aecf3ed15a9d73589d9fdf7181
JozeMen <jozemennon@gmail.com>
2023-05-13 21:01:48 +0400

    Initial commit

---
+++ ConsoleApp1/Category.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    public class Category : IComparable<Category>
+    {
+        // Поля категории
+        public string Product { get; }
+        public string Type { get; }
+        public string Theme { get; }
+        public object HashCode { get; private set; }
+
+        // Конструктор класса
+        public Category(string product, string type, string theme)
+        {
+            Product = product;
+            Type = type;
+            Theme = theme;
+        }
+
+        // Переопределение метода Equals
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            Category other = (Category)obj;
+            return Product == other.Product && Type == other.Type && Theme == other.Theme;
+        }
+
+        // Переопределение метода GetHashCode
+        public override int GetHashCode()
+        {
+            return System.HashCode.Combine(Product, Type, Theme);
+        }
+
+        // Реализация интерфейса IComparable для упорядочивания категорий
+        public int CompareTo(Category other)
+        {
+            // Сначала сравниваем по продукту
+            int productComparison = String.Compare(Product, other.Product, StringComparison.Ordinal);
+            if (productComparison != 0)
+            {
+                return productComparison;
+            }
+
+            // Затем по типу
+            int typeComparison = String.Compare(Type, other.Type, StringComparison.Ordinal);
+            if (typeComparison != 0)
+            {
+                return typeComparison;
+            }
+
+            // Наконец, по теме
+            return String.Compare(Theme, other.Theme, StringComparison.Ordinal);
+        }
+
+        // Реализация оператора равенства
+        public static bool operator ==(Category left, Category right)
+        {
+            if (ReferenceEquals(left, right))
+            {
+                return true;
+            }
+
+            if (left is null || right is null)
+            {
+                return false;
+            }
+
+            return left.Equals(right);
+        }
+
+        // Реализация оператора неравенства
+        public static bool operator !=(Category left, Category right)
+        {
+            return !(left == right);
+        }
+
+        // Реализация оператора меньше
+        public static bool operator <(Category left, Category right)
+        {
+            return left.CompareTo(right) < 0;
+        }
+
+        // Реализация оператора больше
+        public static bool operator >(Category left, Category right)
+        {
+            return left.CompareTo(right) > 0;
+        }
+
+        // Реализация оператора меньше или равно
+        public static bool operator <=(Category left, Category right)
+        {
+            return left.CompareTo(right) <= 0;
+        }
+
+        // Реализация оператора больше или равно
+        public static bool operator >=(Category left, Category right)
+        {
+            return left.CompareTo(right) >= 0;
+        }
+    }
+}
+
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создаем несколько категорий для тестирования
+            Category category1 = new Category("Продукт1", "Тип1", "Тема1");
+            Category category2 = new Category("Продукт2", "Тип1", "Тема2");
+            Category category3 = new Category("Продукт1", "Тип2", "Тема3");
+            Category category4 = new Category("Продукт2", "Тип2", "Тема1");
+            Category category5 = new Category("Продукт1", "Тип1", "Тема1");
+
+            // Добавляем категории в список
+            List<Category> categories = new List<Category>();
+            categories.Add(category1);
+            categories.Add(category2);
+            categories.Add(category3);
+            categories.Add(category4);
+            categories.Add(category5);
+
+            // Сортируем список категорий по возрастанию
+            categories.Sort();
+
+            // Выводим отсортированные категории на экран
+            foreach (Category category in categories)
+            {
+                Console.WriteLine(category.ToString());
+            }
+
+            // Сравниваем категории с помощью операторов сравнения
+            if (category1 == category5)
+            {
+                Console.WriteLine("Категории 1 и 5 эквивалентны.");
+            }
+
+            if (category2 != category3)
+            {
+                Console.WriteLine("Категории 2 и 3 не эквивалентны.");
+            }
+
+            // Задерживаем консоль
+            Console.ReadKey();
+        }
+    }
+}
